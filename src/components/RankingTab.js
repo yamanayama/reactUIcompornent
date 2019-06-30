e6a85b3c82e403548b9722078b29019521eccf1b
Murakami Naomi <38344702+yamanayama@users.noreply.github.com>
2019-06-30 22:38:20 +0900

    コンポーネント

--- src/components/RankingTab.js
+++ src/components/RankingTab.js
@@ -1,47 +1,63 @@
+
 import React, { Component } from 'react';
-// import { render } from 'react-dom';
-// import ReactDOM from 'react-dom';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class RankingTab extends Component {
+
   handleSelect(index, last) {
     console.log('Selected tab: ' + index + ', Last tab: ' + last);
   }
 
   render() {
+
+    const figureStyle = {
+      maxWidth: 300,
+      width: 200,
+      color: '#fff',
+      background: '#ddd'
+    };
+
+    const imgStyle = {
+      width: '100%',
+    };
+
+    const tabs = this.props.tabs.map((tab, index) =>
+      <Tab
+        key={index}> {tab} </Tab>
+    );
+
+    const bodys = this.props.bodys.map((body, index) =>//keyのbodyを指定しないとエラー
+      <TabPanel
+        key={index} >
+        <div>
+          <h2>{body.title}</h2>
+          <figure style={figureStyle}>
+            <img src={body.img} alt={body.title} style={imgStyle} />
+          </figure>
+          <p>{body.text}</p>
+        </div>
+      </TabPanel >
+    );
+
     return (
-
-      <Tabs
-        onSelect={this.handleSelect}
-        selectedIndex={0}
-        className='tab'
-      >
-
-        <TabList
-          activeTabClassName={true}
-          // disabledTabClassName='string'
-          className='tabList'
+      <div className="tabArea">
+        <Tabs
+          onSelect={this.handleSelect}
+          defaultIndex={1}
+          forceRenderTabPanel
+          className='tab'
         >
-          <Tab>Foo</Tab>
-          <Tab>Bar</Tab>
-          <Tab>Baz</Tab>
-        </TabList>
-
-        <TabPanel>
-          <h2>Hello from Foo</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Hello from Bar</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Hello from Baz</h2>
-        </TabPanel>
-
-      </Tabs>
+          <TabList
+            className='tabList'
+          >
+            {tabs}
+          </TabList >
+          {bodys}
+        </Tabs >
+      </div>
     );
   }
 }
 
 export default RankingTab;
-// ReactDOM.render(<RankingTab />, document.getElementById('container'));
